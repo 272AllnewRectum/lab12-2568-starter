1a54248bb1e327a8e30989e9757b9e33e8d321a6
ToiMaiTaiMoi_Tum <iloveusomuchx2@gmail.com>
2025-09-03 00:44:29 +0700

    completed

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -26,8 +26,8 @@ export default function MainLayout() {
       <AppShell.Footer>
         <FooterComponent
           year="2025"
-          fullName="chanadda"
-          studentId="670612039"
+          fullName="Kittiphum Nooudom"
+          studentId="670610674"
         />
       </AppShell.Footer>
 
